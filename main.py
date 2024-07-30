6d6a8296d8bac69cf5ccc8028b72d3926a208d0b
codedreammer <nkanal38@gmail.com>
2024-07-30 08:50:55 +0530

    add the code

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+#method 1 
+from math import sqrt, pi 
+
+result = sqrt(9) * pi 
+print(result)
+
+# method 2 result (9.424)
+import math as m 
+
+result = m.sqrt(9) * m.pi
+print(result)
+'''
+import math
+
+print(dir(math))
+print(math.nan,type(math.nan))
